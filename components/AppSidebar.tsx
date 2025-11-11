b88c197ce0e994766e88e23c192b4342e4887d52
Caio Amaral <147007596+caioamaral-io@users.noreply.github.com>
2025-11-11 11:27:00 -0300

    Dashboard v1

---
+++ components/AppSidebar.tsx
@@ -0,0 +1,66 @@
+import { Home, Inbox, Calendar, Search, Settings } from "lucide-react"
+import { Sidebar, SidebarHeader, SidebarContent, SidebarFooter, SidebarGroupLabel, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarSeparator } from "@/components/ui/sidebar"
+import Link from "next/link";
+import Image from "next/image";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+]
+
+
+const AppSidebar = () => {
+    return (
+      <Sidebar collapsible="icon">
+        <SidebarHeader className="py-4">
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                        <Link href="/">
+                        <Image src="/logo.svg" alt="Logo" width={24} height={24}/>
+                        <span className="text-lg font-regular">dataEdu</span>
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+        <SidebarSeparator />
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span className="text-base font-regular">{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+            {/* Colocar o Icon do Github */}
+        </SidebarFooter>
+      </Sidebar>
+    );
+};
+
+export default AppSidebar
---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { Moon, Settings, Sun, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useSidebar } from "@/components/ui/sidebar";
+
+const Navbar = () => {
+    const {theme, setTheme} = useTheme();
+    const {toggleSidebar} = useSidebar();
+    return (
+        <nav className='p-4 flex items-center justify-between'>
+            {/* Esquerda */}
+            <SidebarTrigger /> 
+            {/* <Button variant="outline" onClick={toggleSidebar}>Custom Button</Button> */}
+            {/* Direita */}
+            <div className="flex items-center gap-4">
+                {/* <Link href="/">Dashboard</Link> */}
+                {/* Theme Dark | Light */}
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                      <span className="sr-only">Toggle theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Avatar>
+                    <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+        </nav>  
+    );
+};
+
+export default Navbar
